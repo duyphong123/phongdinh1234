fd24b6b4520b7962caf345cd93f12b59d2ead3e9
phong <duyphongptit1234@gmail.com>
2019-05-31 21:02:29 +0700

    abc

---
+++ age3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$age =19;
+if($age <18){
+    echo"<br>tre em";
+}
+elseif ($age<30){
+    echo"<br>thanh nien";
+}
+elseif ($age<50){
+    echo"<br>trung nien";
+}
+elseif ($age>50){
+    echo"<br>gia";
+}
+?>
+</body>
+</html>
